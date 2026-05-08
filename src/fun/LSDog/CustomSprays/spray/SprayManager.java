63a13d1f20c15cc5348538da715a28f1ea84b607
LSDog <lsdog@foxmail.com>
2026-05-08 15:28:34 +0800

    Refine spray surface block detection logic
    Signed-off-by: LSDog <LSDog@foxmail.com>

--- src/fun/LSDog/CustomSprays/spray/SprayManager.java
+++ src/fun/LSDog/CustomSprays/spray/SprayManager.java
@@ -273,16 +273,30 @@ public class SprayManager {
     public static boolean isSpraySurfaceBlock(Block block) {
         if (block.isEmpty() || block.isLiquid()) return false;
         Material type = block.getType();
-        String name = type.name();
+        String name = type.name().toUpperCase(Locale.ENGLISH);
+        if (
+                name.startsWith("POTTED_") || // 花盆+植物
+                name.endsWith("CARPET") ||
+                name.endsWith("BUTTON") ||
+                name.endsWith("GATE") ||
+                name.endsWith("FENCE") ||
+                name.endsWith("SIGN") ||
+                name.endsWith("BANNER") ||
+                name.endsWith("CANDLE") ||
+                name.endsWith("PLATE") ||
+                name.endsWith("HEAD") ||
+                name.endsWith("CORAL") ||
+                name.endsWith("VINES") ||
+                name.endsWith("PLANT") ||
+                name.endsWith("CROP")
+        ) return false;
         switch (name) {
-            case "SIGN":
             case "WALL_SIGN":
             case "STRING":
             case "LIGHT":
             case "BUBBLE_COLUMN":
             case "CONDUIT":
             case "KELP":
-            case "KELP_PLANT":
             case "SEAGRASS":
             case "TALL_SEAGRASS":
             case "SEA_PICKLE":
@@ -295,12 +309,16 @@ public class SprayManager {
             case "SOUL_LANTERN":
             case "SOUL_FIRE":
             case "SOUL_WALL_TORCH":
+            case "CORNFLOWER":
+            case "LEAF_LITTER":
+            case "TORCH_FLOWER":
+            case "CACTUS_FLOWER":
+            case "OPEN_EYEBLOSSOM":
+            case "CLOSED_EYEBLOSSOM":
+            case "PINK_PETALS":
+            case "WILDFLOWERS":
             case "WARPED_FUNGUS":
             case "CRIMSON_FUNGUS":
-            case "WEEPING_VINES":
-            case "WEEPING_VINES_PLANT":
-            case "TWISTING_VINES":
-            case "TWISTING_VINES_PLANT":
             case "NETHER_SPROUTS":
             case "CRIMSON_ROOTS":
             case "WARPED_ROOTS":
@@ -311,8 +329,6 @@ public class SprayManager {
             case "GLOW_LICHEN":
             case "FLOWERING_AZALEA":
             case "MOSS_CARPET":
-            case "CAVE_VINES":
-            case "CAVE_VINES_PLANT":
             case "GLOW_BERRIES":
             case "BIG_DRIPLEAF":
             case "BIG_DRIPLEAF_STEM":
@@ -320,257 +336,17 @@ public class SprayManager {
             case "HANGING_ROOTS":
             case "SPORE_BLOSSOM":
             case "CHAIN":
-            case "CANDLE":
             case "BEETROOT_BLOCK":
-            case "PITCHER_PLANT":
-            case "PITCHER_CROP":
-            case "TUBE_CORAL":
-            case "BRAIN_CORAL":
-            case "BUBBLE_CORAL":
-            case "FIRE_CORAL":
-            case "HORN_CORAL":
-            case "DEAD_BRAIN_CORAL":
-            case "DEAD_BUBBLE_CORAL":
-            case "DEAD_FIRE_CORAL":
-            case "DEAD_HORN_CORAL":
-            case "DEAD_TUBE_CORAL":
-            case "TUBE_CORAL_FAN":
-            case "BRAIN_CORAL_FAN":
-            case "BUBBLE_CORAL_FAN":
-            case "FIRE_CORAL_FAN":
-            case "HORN_CORAL_FAN":
-            case "DEAD_TUBE_CORAL_FAN":
-            case "DEAD_BRAIN_CORAL_FAN":
-            case "DEAD_BUBBLE_CORAL_FAN":
-            case "DEAD_FIRE_CORAL_FAN":
-            case "DEAD_HORN_CORAL_FAN":
-            case "WHITE_CANDLE":
-            case "ORANGE_CANDLE":
-            case "MAGENTA_CANDLE":
-            case "LIGHT_BLUE_CANDLE":
-            case "YELLOW_CANDLE":
-            case "LIME_CANDLE":
-            case "PINK_CANDLE":
-            case "GRAY_CANDLE":
-            case "LIGHT_GRAY_CANDLE":
-            case "CYAN_CANDLE":
-            case "PURPLE_CANDLE":
-            case "BLUE_CANDLE":
-            case "BROWN_CANDLE":
-            case "GREEN_CANDLE":
-            case "RED_CANDLE":
-            case "BLACK_CANDLE":
-            case "FENCE":
-            case "OAK_FENCE":
-            case "SPRUCE_FENCE":
-            case "BIRCH_FENCE":
-            case "JUNGLE_FENCE":
-            case "ACACIA_FENCE":
-            case "CHERRY_FENCE":
-            case "DARK_OAK_FENCE":
-            case "MANGROVE_FENCE":
-            case "BAMBOO_FENCE":
-            case "CRIMSON_FENCE":
-            case "WARPED_FENCE":
-            case "NETHER_BRICK_FENCE":
-            case "IRON_FENCE":
-            case "NETHER_FENCE":
-            case "SPRUCE_FENCE_GATE":
-            case "BIRCH_FENCE_GATE":
-            case "JUNGLE_FENCE_GATE":
-            case "DARK_OAK_FENCE_GATE":
-            case "ACACIA_FENCE_GATE":
-            case "OAK_FENCE_GATE":
-            case "CHERRY_FENCE_GATE":
-            case "MANGROVE_FENCE_GATE":
-            case "BAMBOO_FENCE_GATE":
-            case "CRIMSON_FENCE_GATE":
-            case "WARPED_FENCE_GATE":
-            case "STANDING_BANNER":
-            case "WALL_BANNER":
-            case "BANNER":
-            case "WHITE_BANNER":
-            case "ORANGE_BANNER":
-            case "MAGENTA_BANNER":
-            case "LIGHT_BLUE_BANNER":
-            case "YELLOW_BANNER":
-            case "LIME_BANNER":
-            case "PINK_BANNER":
-            case "GRAY_BANNER":
-            case "LIGHT_GRAY_BANNER":
-            case "CYAN_BANNER":
-            case "PURPLE_BANNER":
-            case "BLUE_BANNER":
-            case "BROWN_BANNER":
-            case "GREEN_BANNER":
-            case "RED_BANNER":
-            case "BLACK_BANNER":
-            case "WHITE_WALL_BANNER":
-            case "ORANGE_WALL_BANNER":
-            case "MAGENTA_WALL_BANNER":
-            case "LIGHT_BLUE_WALL_BANNER":
-            case "YELLOW_WALL_BANNER":
-            case "LIME_WALL_BANNER":
-            case "PINK_WALL_BANNER":
-            case "GRAY_WALL_BANNER":
-            case "LIGHT_GRAY_WALL_BANNER":
-            case "CYAN_WALL_BANNER":
-            case "PURPLE_WALL_BANNER":
-            case "BLUE_WALL_BANNER":
-            case "BROWN_WALL_BANNER":
-            case "GREEN_WALL_BANNER":
-            case "RED_WALL_BANNER":
-            case "BLACK_WALL_BANNER":
-            case "CARPET":
-            case "MOOS_CARPET":
-            case "WHITE_CARPET":
-            case "ORANGE_CARPET":
-            case "MAGENTA_CARPET":
-            case "LIGHT_BLUE_CARPET":
-            case "YELLOW_CARPET":
-            case "LIME_CARPET":
-            case "PINK_CARPET":
-            case "GRAY_CARPET":
-            case "LIGHT_GRAY_CARPET":
-            case "CYAN_CARPET":
-            case "PURPLE_CARPET":
-            case "BLUE_CARPET":
-            case "BROWN_CARPET":
-            case "GREEN_CARPET":
-            case "RED_CARPET":
-            case "BLACK_CARPET":
-            case "WOOD_BUTTON":
-            case "STONE_BUTTON":
-            case "POLISHED_BLACKSTONE_BUTTON":
-            case "OAK_BUTTON":
-            case "SPRUCE_BUTTON":
-            case "BIRCH_BUTTON":
-            case "JUNGLE_BUTTON":
-            case "ACACIA_BUTTON":
-            case "CHERRY_BUTTON":
-            case "DARK_OAK_BUTTON":
-            case "MANGROVE_BUTTON":
-            case "BAMBOO_BUTTON":
-            case "CRIMSON_BUTTON":
-            case "WARPED_BUTTON":
             case "SKULL":
-            case "PLAYER_HEAD":
-            case "ZOMBIE_HEAD":
-            case "CREEPER_HEAD":
-            case "DRAGON_HEAD":
-            case "PIGLIN_HEAD":
-            case "PISTON_HEAD":
-            case "ZOMBIE_WALL_HEAD":
-            case "PLAYER_WALL_HEAD":
-            case "CREEPER_WALL_HEAD":
-            case "DRAGON_WALL_HEAD":
-            case "PIGLIN_WALL_HEAD":
-            case "POTTED_TORCHFLOWER":
-            case "POTTED_OAK_SAPLING":
-            case "POTTED_SPRUCE_SAPLING":
-            case "POTTED_BIRCH_SAPLING":
-            case "POTTED_JUNGLE_SAPLING":
-            case "POTTED_ACACIA_SAPLING":
-            case "POTTED_CHERRY_SAPLING":
-            case "POTTED_DARK_OAK_SAPLING":
-            case "POTTED_MANGROVE_PROPAGULE":
-            case "POTTED_FERN":
-            case "POTTED_DANDELION":
-            case "POTTED_POPPY":
-            case "POTTED_BLUE_ORCHID":
-            case "POTTED_ALLIUM":
-            case "POTTED_AZURE_BLUET":
-            case "POTTED_RED_TULIP":
-            case "POTTED_ORANGE_TULIP":
-            case "POTTED_WHITE_TULIP":
-            case "POTTED_PINK_TULIP":
-            case "POTTED_OXEYE_DAISY":
-            case "POTTED_CORNFLOWER":
-            case "POTTED_LILY_OF_THE_VALLEY":
-            case "POTTED_WITHER_ROSE":
-            case "POTTED_RED_MUSHROOM":
-            case "POTTED_BROWN_MUSHROOM":
-            case "POTTED_DEAD_BUSH":
-            case "POTTED_CACTUS":
-            case "POTTED_BAMBOO":
-            case "POTTED_CRIMSON_FUNGUS":
-            case "POTTED_WARPED_FUNGUS":
-            case "POTTED_CRIMSON_ROOTS":
-            case "POTTED_WARPED_ROOTS":
-            case "POTTED_AZALEA_BUSH":
-            case "POTTED_FLOWERING_AZALEA_BUSH":
             case "REPEATER":
             case "COMPARATOR":
+            case "TRIPWIRE":
+            case "TRIPWIRE_HOOK":
             case "REDSTONE_COMPARATOR_ON":
             case "REDSTONE_COMPARATOR_OFF":
             case "REDSTONE_TORCH":
             case "REDSTONE_WALL_TORCH":
             case "REDSTONE_WIRE":
-            case "STONE_PLATE":
-            case "WOOD_PLATE":
-            case "GOLD_PLATE":
-            case "IRON_PLATE":
-            case "STONE_PRESSURE_PLATE":
-            case "POLISHED_BLACKSTONE_PRESSURE_PLATE":
-            case "LIGHT_WEIGHTED_PRESSURE_PLATE":
-            case "HEAVY_WEIGHTED_PRESSURE_PLATE":
-            case "OAK_PRESSURE_PLATE":
-            case "SPRUCE_PRESSURE_PLATE":
-            case "BIRCH_PRESSURE_PLATE":
-            case "JUNGLE_PRESSURE_PLATE":
-            case "ACACIA_PRESSURE_PLATE":
-            case "CHERRY_PRESSURE_PLATE":
-            case "DARK_OAK_PRESSURE_PLATE":
-            case "MANGROVE_PRESSURE_PLATE":
-            case "BAMBOO_PRESSURE_PLATE":
-            case "CRIMSON_PRESSURE_PLATE":
-            case "WARPED_PRESSURE_PLATE":
-            case "OAK_SIGN":
-            case "SPRUCE_SIGN":
-            case "BIRCH_SIGN":
-            case "JUNGLE_SIGN":
-            case "ACACIA_SIGN":
-            case "CHERRY_SIGN":
-            case "DARK_OAK_SIGN":
-            case "MANGROVE_SIGN":
-            case "BAMBOO_SIGN":
-            case "CRIMSON_SIGN":
-            case "WARPED_SIGN":
-            case "OAK_HANGING_SIGN":
-            case "SPRUCE_HANGING_SIGN":
-            case "BIRCH_HANGING_SIGN":
-            case "JUNGLE_HANGING_SIGN":
-            case "ACACIA_HANGING_SIGN":
-            case "CHERRY_HANGING_SIGN":
-            case "DARK_OAK_HANGING_SIGN":
-            case "MANGROVE_HANGING_SIGN":
-            case "BAMBOO_HANGING_SIGN":
-            case "CRIMSON_HANGING_SIGN":
-            case "WARPED_HANGING_SIGN":
-            case "OAK_WALL_SIGN":
-            case "SPRUCE_WALL_SIGN":
-            case "BIRCH_WALL_SIGN":
-            case "ACACIA_WALL_SIGN":
-            case "CHERRY_WALL_SIGN":
-            case "JUNGLE_WALL_SIGN":
-            case "DARK_OAK_WALL_SIGN":
-            case "MANGROVE_WALL_SIGN":
-            case "BAMBOO_WALL_SIGN":
-            case "OAK_WALL_HANGING_SIGN":
-            case "SPRUCE_WALL_HANGING_SIGN":
-            case "BIRCH_WALL_HANGING_SIGN":
-            case "ACACIA_WALL_HANGING_SIGN":
-            case "CHERRY_WALL_HANGING_SIGN":
-            case "JUNGLE_WALL_HANGING_SIGN":
-            case "DARK_OAK_WALL_HANGING_SIGN":
-            case "MANGROVE_WALL_HANGING_SIGN":
-            case "CRIMSON_WALL_HANGING_SIGN":
-            case "WARPED_WALL_HANGING_SIGN":
-            case "BAMBOO_WALL_HANGING_SIGN":
-            case "CRIMSON_WALL_SIGN":
-            case "WARPED_WALL_SIGN":
-            case "CRIMSON_STEM":
-            case "WARPED_STEM":
             case "STRIPPED_CRIMSON_STEM":
             case "STRIPPED_WARPED_STEM":
             case "ATTACHED_PUMPKIN_STEM":
@@ -578,12 +354,9 @@ public class SprayManager {
             case "PUMPKIN_STEM":
             case "MELON_STEM":
             case "VINE":
-            case "TRIPWIRE":
-            case "TRIPWIRE_HOOK":
             case "COCOA":
             case "NETHER_PORTAL":
             case "ENDER_PORTAL":
-            case "CORNFLOWER":
                 return false;
         }
         return !type.isTransparent();
